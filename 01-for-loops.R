17fadbc18219b8da26c2e8e4a20191fcf891aad6
John Franchak <franchak@gmail.com>
2021-01-21 11:09:40 -0800

    automation examples draft

---
+++ 01-for-loops.R
@@ -0,0 +1,53 @@
+#Load packages
+library(tidyverse)
+
+rm(list = ls()) #Clean out workspace
+
+#Get all files in the data_example 1 directory
+file_names <- list.files("data_example_1_2", pattern = ".txt",full.names = T)
+
+#file_names is a character vector, with elements that can be accessed with [1], [2], etc.
+file_names[1]
+
+#We can read in data for each element, but it would be tedious
+ds1 <- read_tsv(file_names[1],  skip = 8, col_names = c("trial", "speed_actual", "speed_faster", "correct"))
+ds2 <- read_tsv(file_names[2],  skip = 8, col_names = c("trial", "speed_actual", "speed_faster", "correct"))
+ds3 <- read_tsv(file_names[3],  skip = 8, col_names = c("trial", "speed_actual", "speed_faster", "correct"))
+#...and so on
+
+#Any time we are iterating over a vector, a for loop can automate things
+#for every (*thing* in *list of things*) do {actions in a loop}
+for (file in file_names) {
+  print(file)
+}
+
+#Now let's do something with each file name
+
+#First, let's set up a place to put the data as it comes in
+#Create a tibble with the right structure, then use filter(FALSE) to delete all the data
+ds <- read_tsv(file_names[1],  skip = 8, col_names = c("trial", "speed_actual", "speed_faster", "correct"))
+ds <- ds %>% filter(FALSE)
+
+#Loop through file names
+for (file in file_names) {
+  #Read the new data into a temporary dataset
+  temp_ds <- read_tsv(file,  skip = 8, col_names = c("trial", "speed_actual", "speed_faster", "correct"))
+  #Bind (append) the new data to the dataset
+  ds <- bind_rows(ds, temp_ds)
+}
+
+#What did we do wrong?
+
+#Redo the example above, but now save "file" into the data frame as an identifier
+ds <- read_tsv(file_names[1],  skip = 8, col_names = c("trial", "speed_actual", "speed_faster", "correct"))
+ds$file <- "file"
+ds <- ds %>% filter(FALSE)
+
+#Loop through file names
+for (file in file_names) {
+  #Read the new data into a temporary dataset
+  temp_ds <- read_tsv(file,  skip = 8, col_names = c("trial", "speed_actual", "speed_faster", "correct"))
+  temp_ds$file <- file
+  #Bind (append) the new data to the dataset
+  ds <- bind_rows(ds, temp_ds)
+}
---
+++ 02-map.R
@@ -0,0 +1,35 @@
+#Load packages
+library(tidyverse)
+library(here)
+
+rm(list = ls()) #Clean out workspace
+
+file_names <- list.files("data_example_1_2", pattern = ".txt",full.names = T)
+
+#map applies a function to each element of a list or vector
+map(file_names, toupper)
+
+#map let's you define functions on the fly using ~, where '.x' serves as the item from the list/vector
+map(file_names, ~toupper(.x))
+
+#Use map to get the full file path of each "short file" name
+file_names_short <- list.files("data_example_1_2", pattern = ".txt",full.names = F)
+map(file_names, ~here("data_example_1_2",.x))
+
+#This isn't quite what we want. Map returns a list by default
+ds <- map(file_names, ~ read_tsv(.x,  skip = 8, col_names = c("trial", "speed_actual", "speed_faster", "correct")))
+ds <- bind_rows(ds) #This fixes it, but there's an easier way
+
+#Map dfr is a variant of map that returns a dataframe
+ds <- map_dfr(file_names, ~ read_tsv(.x,  skip = 8, col_names = c("trial", "speed_actual", "speed_faster", "correct")) %>% mutate(file = .x))
+
+#Can we make this a bit cleaner?
+
+#Make a custom function that reads the data and adds the filename to it
+read_our_data <- function(fname) {
+  data <- read_tsv(fname,  skip = 8, col_names = c("trial", "speed_actual", "speed_faster", "correct"))
+  data <- data %>% mutate(file = fname)
+}
+
+#Map file_names to our new function
+ds <- map_dfr(file_names, ~ read_our_data(.x))
